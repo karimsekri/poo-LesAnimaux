79e79366255d23dc4c221419d9d09a063682d949
karim <karim.sekri@gmail.com>
2023-10-02 08:58:59 +0200

    version final

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import { json } from "stream/consumers";
 import { Bovin } from "./animaux/Bovin";
 import { Canide } from "./animaux/Canide";
 import { Chien } from "./animaux/Chien";
 import { Felin } from "./animaux/Felin";
 import { Ovin } from "./animaux/Ovin";
+
+import axios from 'axios';
+import xml2js from 'xml2js';
+import HTMLParser from 'html-to-json-parser';
 
 
 const chien1 =  new Chien("Rex",2,"BergerAllemand")
@@ -19,4 +24,15 @@ mouton1.cri();
 const vache1 = new Bovin("Vache", 9);
 console.log(vache1);
 
-
+// axios.get('https://www.data.gouv.fr/fr/datasets/r/70967da2-234e-4583-8ff1-5ce885b1acb3')
+//   .then(response => {
+//     //console.log(response.data);
+//     const mesDatas = response.data;
+//     console.log(mesDatas);
+//    xml2js.parseString(mesDatas, (err, result) => {
+//      console.log(result);
+//    });
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
